34c41323ebbcc42a4ccaff751db2010c67fd118f
schipkovalina <31881656+schipkovalina@users.noreply.github.com>
2017-09-23 20:18:13 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,15 @@
+a = int (input("Введите число a"))
+b = int (input("Введите число b"))
+c = int (input("Введите число c"))
+if a % b == c:
+    print ('YES, a % b == c')
+else:
+    print ('NO, a % b != c')
+    
+if a / b == c:
+    print ('YES, a / b == c')
+else:
+    print ('NO, a / b != c')
+    
+    
+
