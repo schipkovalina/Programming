20ed35b2ff63fbe54a558409a0bb7c7dd7cdfc03
schipkovalina <31881656+schipkovalina@users.noreply.github.com>
2017-12-04 19:30:04 +0300

    Update and rename homework5.py to hw5/homework5.py

--- homework5.py
+++
@@ -1,8 +0,0 @@
-n = 0
-with open("text.txt", 'w', encoding='utf-8') as f:
-    s = input('Введите слово: ')
-    while s:
-        n += 1
-        s = s[n::1]
-        f.write(s + '\n')
-        s = input('Введите слово: ')
