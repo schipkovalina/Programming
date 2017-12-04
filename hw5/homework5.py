13b4db9fe0bb3abce30902017888a6f148c33b9a
schipkovalina <31881656+schipkovalina@users.noreply.github.com>
2017-12-04 19:33:28 +0300

    Update homework5.py

--- hw5/homework5.py
+++ hw5/homework5.py
@@ -5,5 +5,5 @@ with open("text.txt", "w", encoding="utf-8") as f:
     while s:
         n += 1
         s = s[n::1]
-        f.write(s + '\n')
+        f.write(s + "\n")
         s = input("Введите слово:")
