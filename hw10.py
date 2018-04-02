a7e4fbd33646e69a9a58e96778a51198a13fb3ea
schipkovalina <31881656+schipkovalina@users.noreply.github.com>
2018-04-02 19:57:12 +0300

    Add files via upload

---
+++ hw10.py
@@ -0,0 +1,18 @@
+#Вариант 3
+
+import re
+
+def openfile():
+    with open("./wiki.html", "r", encoding="utf-8") as f:
+        for line in f:
+            s = re.search('data-wikidata-property-id="P571".*>(\d+)', line)
+            if s:
+                data = s.group(1)
+                return data
+            
+def write(year):
+    with open("./text.txt", "w", encoding="utf-8") as t:
+        result = "Год основания:" + year
+        t.write(result)
+        
+write(openfile())
