e1d7b7e50fd60d90a85a7354ce5e7312b13a0358
schipkovalina <31881656+schipkovalina@users.noreply.github.com>
2018-04-09 19:45:33 +0300

    Update hw11.py

--- hw11/hw11.py
+++ hw11/hw11.py
@@ -5,8 +5,6 @@ import re
 def openfile():
     with open( "./linguistics.html", "r", encoding="utf-8") as f:
         article = f.read()
-#        result = re.sub('язык(а|у|ом|е|и|ов|ам|ами|ах)?', 'шашлык(а|у|ом|е|и|ов|ам|ами|ах)?', article)
-#        result = re.sub('Язык(а|у|ом|е|и|ов|ам|ами|ах)?', 'Шашлык(а|у|ом|е|и|ов|ам|ами|ах)?', result)
         result = re.sub(r'(\W|^)язык(а|у|ом|е|и|ов|ам|ами|ах)?(-|\W|$)', r'\1шашлык\2\3', article)
         result = re.sub(r'(\W|^)Язык(а|у|ом|е|и|ов|ам|ами|ах)?(-|\W|$)', r'\1Шашлык\2\3', result)
         return result
